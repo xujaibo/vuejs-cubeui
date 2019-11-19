ea9b6cd61d54179e3b29a04312c6c9e29adb7989
xujiabo <xujp@oceansoft.com.cn>
2019-11-19 17:32:23 +0800

    项目初始化

---
+++ vue.config.js
@@ -0,0 +1,51 @@
+const appData=require('./data.json')
+const seller=appData.seller
+const goods=appData.goods
+const ratings=appData.ratings
+
+
+module.exports = {
+    css: {
+        loaderOptions: {
+            stylus: {
+                'resolve url': true,
+                'import': [
+                    './src/theme'
+                ]
+            }
+        }
+    },
+    pluginOptions: {
+        'cube-ui': {
+            postCompile: true,
+            theme: true
+        }
+    },
+    devServer:{
+        before(app){
+            app.get('/api/seller',function (req,res) {
+                res.json({
+                    errno:0,
+                    data:seller
+                })
+            })
+            app.get('/api/ratings',function (req,res) {
+                res.json({
+                    errno:0,
+                    data:ratings
+                })
+            })
+            app.get('/api/goods',function (req,res) {
+                res.json({
+                    errno:0,
+                    data:goods
+                })
+            })
+        }
+    },
+    chainWebpack(config){
+        config.resolve.alias
+            .set('@api',resolve('src/api'))
+    }
+
+}
